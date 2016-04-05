2fa9059cc83dfbd0070d709863947f9137e17d4f
Tyler Bonnell <tbonnell@uw.edu>
2016-04-05 00:21:15 -0700

    something that actually works wow

---
+++ Assets/DragCueBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragCueBall : MonoBehaviour {
+	
+	float startTime;
+	Vector3 startPos;
+	Rigidbody rb;
+	public float factor = 100;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void OnMouseDown() {
+		startTime = Time.time;
+		startPos = Input.mousePosition;
+	}
+
+	void OnMouseUp() {
+		Vector3 endPos = Input.mousePosition;
+		Vector3 force = endPos - startPos;
+		force.z = -force.x;
+		force.x = force.y;
+		force.y = 0;
+
+		Debug.Log (force);
+		rb.AddForce(force * factor);
+	}
+}

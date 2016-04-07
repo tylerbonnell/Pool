9ad116eaf9cb34314247b95b64175812e68a6ff8
Tyler Bonnell <tbonnell@uw.edu>
2016-04-07 00:33:49 -0700

    random rotation of ballz

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class BallScript : MonoBehaviour {
 
+	void Start() {
+		transform.rotation = Quaternion.Euler (Random.Range (0, 360), 
+		                                       Random.Range (0, 360), 
+		                                       Random.Range (0, 360));
+	}
+	
 	void Update() {
 		Vector3 pos = transform.position;
 		pos.y = Mathf.Min(-0.1f, pos.y);
--- Assets/Rack.cs
+++ Assets/Rack.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Rack : MonoBehaviour {
 
+	public Material[] materials;
+
 	// Use this for initialization
 	void Start () {
-	
+		ArrayList mats = new ArrayList (materials);
 	}
 	
 	// Update is called once per frame

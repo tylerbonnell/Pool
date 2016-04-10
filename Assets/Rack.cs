823809a34e0a4895cbc4f3c022537ad0c989cbce
Tyler Bonnell <tbonnell@uw.edu>
2016-04-09 20:03:58 -0700

    fixed corners

--- Assets/Rack.cs
+++ Assets/Rack.cs
@@ -13,19 +13,27 @@ public class Rack : MonoBehaviour {
 	void Start () {
 		ArrayList mats = new ArrayList (solids);
 		mats.AddRange(stripes);
-		foreach (Material m in mats) {
-			Debug.Log(m);
-		}
 
 		Material cornerStripe = stripes[Random.Range (0, stripes.Length - 1)];
 		Material cornerSolid = solids[Random.Range (0, solids.Length - 1)];
 		mats.Remove (cornerSolid);
 		mats.Remove (cornerStripe);
+
+		// Make the corners one solid, one stripe
+		if (Random.Range(0, 2) == 0) {
+			leftBall.GetComponent<Renderer> ().material = cornerSolid;
+			rightBall.GetComponent<Renderer> ().material = cornerStripe;
+		} else {
+			leftBall.GetComponent<Renderer> ().material = cornerStripe;
+			rightBall.GetComponent<Renderer> ().material = cornerSolid;
+		}
+
 		foreach (GameObject go in balls) {
 			Material m = (Material) mats[Random.Range(0, mats.Count - 1)];
 			go.GetComponent<Renderer>().material = m;
 			mats.Remove(m);
 		}
+
 	}
 	
 	// Update is called once per frame
